81b62585af1f45f9b3589d87b5105a34042c82bc
Jan Jelínek <jan.jelinek@unicorn.com>
2022-09-21 19:55:17 +0200

    create fetch data context component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  return <div className="App">hello</div>;
 }
 
 export default App;
