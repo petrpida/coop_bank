8fe7853ad74a308e25ac87c73b0496a7e8ebe563
Jan Jelínek <jan.jelinek@unicorn.com>
2022-09-22 22:39:37 +0200

    style changes - calculator

--- client/src/routes/Calculator.js
+++ client/src/routes/Calculator.js
@@ -57,30 +57,39 @@ const Calculator = React.memo(() => {
     <div className={styles.inputContainer}>
       <div>
         <FormGroup>
-          <Form.Label>Kolik si chcete půjčit?</Form.Label>
+          <div className={styles.values}>
+            <Form.Label>Kolik si chcete půjčit?</Form.Label>
+            <span className={styles.outputsValue}>
+              {new Intl.NumberFormat("cs-CZ").format(amount) + " Kč"}
+            </span>
+          </div>
           <input
             defaultValue={350000}
             type="range"
             className="form-range"
             min="5000"
             max="1200000"
-            step="1000"
+            step={amount < 100000 ? "1000" : "5000"}
             id="customRange3"
             onChange={(event) => {
               setAmount(event.target.value);
               set(amount, months);
             }}
           />
-          <div className={styles.outputs}>
-            <span className={styles.outputsValue}>
-              {new Intl.NumberFormat("cs-CZ").format(amount) + " Kč"}
-            </span>
+          <div className={styles.values}>
+            <p>5 000 Kč</p>
+            <p>1 200 000 Kč</p>
           </div>
         </FormGroup>
       </div>
       <div>
         <FormGroup>
-          <Form.Label>Jak dlouho chcete splácet?</Form.Label>
+          <div className={styles.values}>
+            <Form.Label>Jak dlouho chcete splácet?</Form.Label>
+            <span className={styles.outputsValue}>
+              {getYearsAndMonths(months)}
+            </span>
+          </div>
           <input
             defaultValue={24}
             type="range"
@@ -94,26 +103,38 @@ const Calculator = React.memo(() => {
               set(amount, months);
             }}
           />
-          <div className={styles.outputs}>
-            <span className={styles.outputsValue}>
-              {getYearsAndMonths(months)}
-            </span>
+          <div className={styles.values}>
+            <p>6 měsíců</p>
+            <p>5 let</p>
           </div>
         </FormGroup>
       </div>
       <div className={styles.outputCard}>
-        <Card className="text-center rounded-0">
-          <Card.Header>Vaše výhodná půjčka</Card.Header>
+        <Card
+          className="text-center rounded-0"
+          style={{ border: "2px solid #00843D" }}
+        >
+          <Card.Header className={styles.header}>
+            Vaše výhodná půjčka
+          </Card.Header>
           {calculatedData.state === "success" && (
             <Card.Body>
-              <Card.Title>{`Měsíční splátka ${new Intl.NumberFormat(
-                "cs-CZ"
-              ).format(calculatedData.data.monthlyPayment)} Kč`}</Card.Title>
-              <Card.Text>{`Roční úroková sazba ${calculatedData.data.yearlyInterest}%`}</Card.Text>
-              <Card.Text>{`RPSN ${calculatedData.data.RPSN}%`}</Card.Text>
-              <Card.Text>{`Celkově zaplatíte ${new Intl.NumberFormat(
-                "cs-CZ"
-              ).format(calculatedData.data.overallAmount)} Kč`}</Card.Text>
+              <Card.Title
+                className={styles.title}
+              >{`Měsíční splátka ${new Intl.NumberFormat("cs-CZ").format(
+                calculatedData.data.monthlyPayment
+              )} Kč`}</Card.Title>
+              <Card.Text
+                className={styles.text}
+              >{`Roční úroková sazba ${calculatedData.data.yearlyInterest}%`}</Card.Text>
+              <Card.Text
+                className={styles.text}
+              >{`RPSN ${calculatedData.data.RPSN}%`}</Card.Text>
+              <Card.Text
+                className={styles.text}
+              >{`Celkově zaplatíte ${new Intl.NumberFormat("cs-CZ").format(
+                calculatedData.data.overallAmount
+              )} Kč`}</Card.Text>
               {amount > 200000 && (
                 <p className={styles.fee}>
                   {`Poplatek za sjednání smlouvy ${new Intl.NumberFormat(
